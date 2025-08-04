bonktree/tayga

commit d953a6106d12a223d3a6f78e7d7c94630bcb073b

Improve Systemd Units for all distros (#95)
* Update Systemd unit

* Update Systemd unit

* Another round of changes

* typo

* Use systemd StateDirectory

* Move service units to a new folder

Makefile
@@ -7,12 +7,13 @@ SOURCES := nat64.c addrmap.c dynamic.c tayga.c conffile.c
 #Default installation paths (may be overridden by environment variables)
 prefix ?= /usr/local
 exec_prefix ?= $(prefix)
-DESTDIR ?= /tmp
+DESTDIR ?=
 sbindir ?= $(exec_prefix)/sbin
 datarootdir ?= $(prefix)/share
 mandir ?= $(datarootdir)/man
 INSTALL_DATA ?= install -m 644
 INSTALL_PROGRAM ?= install -m 755
+SYSTEMCTL ?= /bin/systemctl
 OPENRC ?= /sbin/rc-service
 sysconfdir ?= /etc
 localstatedir ?= /var
@@ -38,13 +39,23 @@ ifndef RELEASE
 endif
 	$(CC) $(CFLAGS) -o tayga $(SOURCES) $(LDFLAGS) -static
 
+.PHONY: clean
 clean:
 	$(RM) tayga version.h
 
+# Install will create sbindir and mandir(s)
+# Install tayga and man pages
+# If systemd is detected, copy systemd service file
+# If systemd is detected and conf does not already exist, copy example conf file
+.PHONY: install
 install: $(TARGET)
 	-mkdir -p $(DESTDIR)$(sbindir) $(DESTDIR)$(mandir)/man5 $(DESTDIR)$(mandir)/man8
 	$(INSTALL_PROGRAM) tayga $(DESTDIR)$(sbindir)/tayga
 	$(INSTALL_DATA) tayga.8 $(DESTDIR)$(mandir)/man8
 	$(INSTALL_DATA) tayga.conf.5 $(DESTDIR)$(mandir)/man5
+	setcap CAP_NET_ADMIN+ep $(DESTDIR)$(sbindir)/tayga
+	if test -x "$(SYSTEMCTL)" && test -d "$(DESTDIR)$(sysconfdir)/systemd/system"; then $(INSTALL_DATA) scripts/tayga@.service $(DESTDIR)$(sysconfdir)/systemd/system/tayga@.service && $(SYSTEMCTL) daemon-reload; fi
+	if test -x "$(SYSTEMCTL)" && test ! -e "$(DESTDIR)$(sysconfdir)/tayga/default.conf"; then mkdir -p $(DESTDIR)$(sysconfdir)/tayga && $(INSTALL_DATA) tayga.conf.example $(DESTDIR)$(sysconfdir)/tayga/default.conf ; fi
 	if test -x "$(OPENRC)" && test -d "$(DESTDIR)$(sysconfdir)/init.d/"; then $(INSTALL_PROGRAM) scripts/tayga.initd $(DESTDIR)$(sysconfdir)/init.d/tayga && $(INSTALL_DATA) scripts/tayga.confd $(DESTDIR)$(sysconfdir)/conf.d/tayga ; fi
 	if test -x "$(OPENRC)" && test ! -e "$(DESTDIR)$(sysconfdir)/tayga.conf"; then $(INSTALL_DATA) tayga.conf.example $(DESTDIR)$(sysconfdir)/tayga.conf ; fi
+  

conffile.c
@@ -595,6 +595,36 @@ void config_validate(void)
 		exit(1);
 	}
 
+	/* Check if the env var STATE_DIRECTORY exists to use as data_dir
+	 * This env var is set by systemd
+	 * And it can still be overridden by the conf file
+	 */
+	char * sd = getenv("STATE_DIRECTORY");
+	if(sd && !gcfg->data_dir[0]) {
+		if (sd[0] != '/') {
+			slog(LOG_CRIT, "Error: STATE_DIRECTORY must be an "
+				"absolute path\n");
+			exit(1);
+		}
+		/* Copy env var into data_dir */
+		if(strlen(sd) + 1 > sizeof(gcfg->data_dir)) {
+			slog(LOG_CRIT, "STATE_DIRECTORY env var is too long, "
+					"aborting...\n");
+			exit(1);
+		}
+		/* Check for a : which signifies that we have multiple dirs */
+		for(int i = 0; sd[i]; i++) {
+			if(sd[i] == ':') {
+				slog(LOG_WARNING, "STATE_DIRECTORY env var contains "
+						"multiple directories, using first one\n");
+				sd[i] = 0;
+				break;
+			}
+		}
+		/* Copy state directory */
+		strcpy(gcfg->data_dir, sd);
+	}
+
 	m4 = list_entry(gcfg->map4_list.next, struct map4, list);
 	m6 = list_entry(gcfg->map6_list.next, struct map6, list);
 

docs/nat64/nat64-dynpool.conf
@@ -7,7 +7,7 @@ ipv4-addr 192.168.240.1
 # IPv6 address of Tayga (may only be within pref64 if pref64 is not the well-known prefix)
 ipv6-addr 2001:db8:beef:6464::1
 # Data Dir to store dynamic pool mappings
-data-dir /var/spool/tayga
+data-dir /var/lib/tayga
 # Disable strict well-known prefix checking to use example IPs
 #wkpf-strict no
 # pref64

scripts/tayga@.service
@@ -1,0 +1,25 @@
+[Unit]
+Description=Simple, no-fuss NAT64
+After=network.target
+
+[Service]
+# To set up an extra tayga service instance, create a new tayga config in
+# /etc/tayga/instancename.conf
+# /etc/tayga/instancename.conf
+# systemctl enable tayga@instancename.service
+NoNewPrivileges=true
+ProtectSystem=full
+ProtectHome=true
+ProtectHostname=true
+ProtectKernelTunables=true
+ProtectKernelModules=true
+ProtectKernelLogs=true
+ProtectControlGroups=true
+RestrictRealtime=true
+StateDirectory=tayga
+PrivateTmp=true
+Type=forking
+ExecStart=tayga --config /etc/tayga/%i.conf
+
+[Install]
+WantedBy=multi-user.target

tayga.conf.5
@@ -162,7 +162,9 @@ command-line option.
 .IP
 The
 .B data-dir
-directive is optional, but without it, dynamic mappings will be lost when the
+directive is optional. If not configured, TAYGA will check the environment variable 
+.B STATE_DIRECTORY.
+If neither is provided, dynamic mappings will be lost when the
 TAYGA daemon is stopped.  Also, use of the
 .B \-\-chroot
 command-line option will not be possible.

tayga.conf.example
@@ -1,5 +1,5 @@
 #
-# Sample configuration file for TAYGA 0.9.2
+# Sample configuration file for TAYGA 0.9.6
 #
 # Modify this to use your own addresses!!
 #
@@ -91,9 +91,12 @@ dynamic-pool 192.168.255.0/24
 # directory.  Omit if you do not need these maps to be persistent between
 # instances of TAYGA.
 #
+# Tayga will also respect the environment variable STATE_DIRECTORY if
+# this configuration flag is not set
+#
 # Optional.
 #
-data-dir /var/spool/tayga
+#data-dir /var/lib/tayga
 
 #
 # Establishes a single-host map.  If an IPv6 host should be consistently

tayga.service
@@ -1,11 +1,0 @@
-[Unit]
-Description=Simple, no-fuss NAT64
-After=network.target
-
-[Service]
-Type=simple
-PrivateTmp=true
-ExecStart=/usr/sbin/tayga -d --config /etc/tayga.conf
-
-[Install]
-WantedBy=multi-user.target

tayga@.service
@@ -1,16 +1,0 @@
-[Unit]
-Description=Simple, no-fuss NAT64
-After=network.target
-
-[Service]
-# To set up an extra tayga service instance, create a new tayga config in
-# /etc/tayga/instancename.conf. Then enable this config as a systemd alias
-#
-# systemctl enable tayga@instancename.service
-
-Type=simple
-PrivateTmp=true
-ExecStart=/usr/sbin/tayga -d --config /etc/tayga/%i.conf
-
-[Install]
-WantedBy=multi-user.target

commit d453e8f51dcada470e30da00793d0ba4d3cccceb

Various fixes to packaging workflow (#127)
* Makefile: move sysadmin side effects out of `make install`

The install recipe is overloaded with unrelated actions which make no
sense in an isolated, reproducible build environment.

For example, it tries to reload a systemd service manager
if the systemctl binary was present, or to set file capabilities.
Neither are possible in an isolated package build environment: there is
no service manager to reload, and there are insufficient privileges to
setcap +ep anything, so this is not only nonsensical, but fails `make
install`. Both of these actions pertain to system administration, not
release build or packaging; they are useful on a real running system or
in a development workflow.

We introduce a new target, `make install-live`, to do what `make install`
previously did on a live system, and move the setcap and systemctl
invocations to this target.

The result of this, to put it briefly, is that the following command
just works without privilege escalation:
    DESTDIR="$buildroot" make install
as it should.

Fixes: d953a6106d12 ("Improve Systemd Units for all distros (#95)")

* Makefile: make path to unitdir overridable

In the install recipe, this also saves us from repeating ourselves a bit.

* Makefile: drop the unset/unused TARGET variable

Fixes: 28030e95e683 ("Preliminary Github Actions (#86)")

Makefile
@@ -24,6 +24,7 @@ OPENRC ?= /sbin/rc-service
 sysconfdir ?= /etc
 localstatedir ?= /var
 SUDO ?= /usr/bin/sudo
+systemdsystemunitdir ?= $(sysconfdir)/systemd/system
 
 # Compile Tayga
 .PHONY: all
@@ -80,16 +81,19 @@ clean:
 # Install tayga and man pages
 # If systemd is detected, copy systemd service file
 # If systemd is detected and conf does not already exist, copy example conf file
-.PHONY: install
-install: $(TARGET)
+.PHONY: install install-live
+install:
 	-mkdir -p $(DESTDIR)$(sbindir) $(DESTDIR)$(mandir)/man5 $(DESTDIR)$(mandir)/man8
 	$(INSTALL_PROGRAM) tayga $(DESTDIR)$(sbindir)/tayga
 	$(INSTALL_DATA) tayga.8 $(DESTDIR)$(mandir)/man8
 	$(INSTALL_DATA) tayga.conf.5 $(DESTDIR)$(mandir)/man5
-	setcap CAP_NET_ADMIN+ep $(DESTDIR)$(sbindir)/tayga
-	if test -x "$(SYSTEMCTL)" ; then mkdir -p $(DESTDIR)$(sysconfdir)/systemd/system && $(INSTALL_DATA) scripts/tayga@.service $(DESTDIR)$(sysconfdir)/systemd/system/tayga@.service && $(SYSTEMCTL) daemon-reload; fi
+	if test -x "$(SYSTEMCTL)" ; then mkdir -p $(DESTDIR)$(systemdsystemunitdir) && $(INSTALL_DATA) scripts/tayga@.service $(DESTDIR)$(systemdsystemunitdir)/tayga@.service; fi
 	if test -x "$(SYSTEMCTL)" && test ! -e "$(DESTDIR)$(sysconfdir)/tayga/default.conf"; then mkdir -p $(DESTDIR)$(sysconfdir)/tayga && $(INSTALL_DATA) tayga.conf.example $(DESTDIR)$(sysconfdir)/tayga/default.conf ; fi
 	if test -x "$(OPENRC)"; then mkdir -p $(DESTDIR)$(sysconfdir)/init.d && $(INSTALL_PROGRAM) scripts/tayga.initd $(DESTDIR)$(sysconfdir)/init.d/tayga ; fi
 	if test -x "$(OPENRC)" && test ! -e "$(DESTDIR)$(sysconfdir)/conf.d/tayga"; then mkdir -p $(DESTDIR)$(sysconfdir)/conf.d &&$(INSTALL_DATA) scripts/tayga.confd $(DESTDIR)$(sysconfdir)/conf.d/tayga  ; fi
 	if test -x "$(OPENRC)" && test ! -e "$(DESTDIR)$(sysconfdir)/tayga.conf"; then $(INSTALL_DATA) tayga.conf.example $(DESTDIR)$(sysconfdir)/tayga.conf ; fi
-  
+
+# Install the artifacts on a running system.
+install-live: install
+	setcap CAP_NET_ADMIN+ep $(DESTDIR)$(sbindir)/tayga
+	if test -d "/run/systemd/system" && test -x "$(SYSTEMCTL)"; then $(SYSTEMCTL) daemon-reload; fi